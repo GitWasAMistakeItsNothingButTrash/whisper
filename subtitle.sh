906e955ccdd93f991532f5ce51901226629efed2
GitWasAMistakeItsNothingButTrash <108470862+GitWasAMistakeItsNothingButTrash@users.noreply.github.com>
2023-04-16 21:16:27 +0000

    Update subtitle.sh

--- subtitle.sh
+++ subtitle.sh
@@ -2,9 +2,8 @@
 
 # Install requisites
 sudo apt update
-sudo apt install git ffmpeg python3-pip
+sudo apt install git ffmpeg python3-pip gawk
 pip install git+https://github.com/openai/whisper.git
-pip install python-docx
 
 # Define variables
 read -p "Please enter the absolute path to the directory (include a slash at the end): " path2dir
@@ -16,18 +15,30 @@ read -p "How many CPU threads can Whisper use? (Do not exceed total threads):" t
 # Speech-to-text transcription
 ~/.local/bin/whisper --model large-v2 --output_dir "$path2dir" --output_format vtt --task transcribe --language $language --threads $threadnumber "$path2dir$inputfile.mp4"
 
+# Remove header
+gawk -i inplace 'NR > 2' "$path2dir$inputfile.vtt"
+
 # Split timestamps and transcription
-python3 $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/split.py "$path2dir" "$inputfile"
+gawk -i inplace 'NR % 3 == 1' "$path2dir$inputfile.vtt" > "$path2dir"timestamps.tmp
+gawk -i inplace 'NR % 3 == 2' "$path2dir$inputfile.vtt" > "$path2dir"transcription.tmp
 
 # Translate transcription
+"$path2dir"transcription.tmp --> "$path2dir"translation.tmp
 
 # Merge timestamps and translation
-python3 $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/merge.py "$path2dir" "$outputfile"
+paste -d \\n "$path2dir"timestamps.tmp "$path2dir"translation.tmp > "$path2dir$outputfile.vtt" 
 
 # Clean up temporary files
-rm -f "$path2dir$timestamps.docx"
-rm -f "$path2dir$transcription.docx"
-rm -f "$path2dir$translation.docx"
+rm -f "$path2dir"timestamps.tmp
+rm -f "$path2dir"transcription.tmp
+rm -f "$path2dir"translation.tmp
+
+# Insert empty line every third line:
+gawk -i inplace ' {print;} NR % 2 == 0 { print ""; }' "$path2dir$outputfile.vtt"
+
+# Restore header
+gawk -i inplace 'NR == 1 { print "WEBVTT"; }' "$path2dir$outputfile.vtt"
+gawk -i inplace 'NR == 2 { print ""; }' "$path2dir$outputfile.vtt"
 
 # Burn subtitles into video
 ffmpeg -i "$path2dir$inputfile.mp4" -vf subtitles="$path2dir$outputfile.vtt" "$path2dir$outputfile.mp4"
